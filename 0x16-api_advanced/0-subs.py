d071d906bfd93f88eb7258fccc5f28fb28abf8f0
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-03-05 18:58:06 +0100

    Tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,5 +8,5 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    r = requests.get(url=url, headers=headers).json()
+    r = requests.get(url=url, headers=headers, allow_redirects=False).json()
     return 0 if 'error' in r else r['data']['subscribers']
