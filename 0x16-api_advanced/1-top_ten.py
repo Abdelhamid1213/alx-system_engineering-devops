01c6b95ac527658537ec66898a44f714e092172d
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-03-05 22:12:21 +0100

    Tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,14 +3,22 @@
 import requests
 
 
+http_proxy = "http://173.249.29.243:9123"
+
+proxies = {"http": http_proxy}
+
+
 def top_ten(subreddit):
     """Prints the titles of the top ten posts from a given subreddit."""
     headers = {"User-Agent": "ubuntu:alx/1.0"}
     url = f'https://old.reddit.com/r/{subreddit}/hot.json?limit=10'
 
-    r = requests.get(url=url, headers=headers, allow_redirects=False)
+    r = requests.get(url=url, headers=headers, allow_redirects=False,
+                     proxies=proxies)
     if r.status_code == 404:
         print(None)
     else:
         for post in r.json()['data']['children']:
             print(post['data']['title'])
+
+top_ten('linux')
