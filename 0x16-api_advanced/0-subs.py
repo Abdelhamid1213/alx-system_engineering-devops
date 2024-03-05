90505c5764d341edf4d4281929b27e100ff5b500
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-03-05 19:52:43 +0100

    Tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This module contains a function to retrieve the number of subscribers for
- a given subreddit."""
+"""retrieve the number of subscribers for a given subreddit."""
 import requests
 
 
