dc42ba614adbc07233ebf1878bb5891a09e5ad68
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-03-06 16:37:02 +0100

    Tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # automated puppet fix (to find out why Apache is returning a 500 error)
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  replace => 'true',
-  content => file('/var/www/html/wp-settings.php').content.gsub('.phpp', '.php'),
-  notify  => Service['apache2'],
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
