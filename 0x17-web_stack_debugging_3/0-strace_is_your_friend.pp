bc8de92f09839cdd0fcc4083118e6684d8478146
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-03-06 16:34:54 +0100

    Tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# Check for incorrect extension before modification
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
 file { '/var/www/html/wp-settings.php':
   ensure => present,
   require => Exec['VerifyExtension'],
